772dd1cef7fb16fa3f5ee8e94befc4e833842f6f
QuUteO <quuteo86@gmail.com>
2026-05-28 12:49:34 +0300

    binary_search

---
+++ binary_search/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func binary_search(array []int, value int) int {
+	first := 0
+	last := len(array) - 1
+
+	for first <= last {
+		mid := (first + last) / 2
+
+		if array[mid] == value {
+			return mid
+		} else if array[mid] > value {
+			last = mid - 1
+		} else {
+			first = mid + 1
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	fmt.Println(binary_search([]int{1, 2, 3, 4, 5}, 1))
+}
